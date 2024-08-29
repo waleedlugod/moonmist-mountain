f9388a5967beffdbf9063d1d12b470c3e36d8c5f
Waleed Lugod <lugodwaleed@gmail.com>
2024-08-29 22:55:10 +0800

    Setup basic environement

--- Characters/player.gd
+++ Characters/player.gd
@@ -5,11 +5,11 @@ const SPEED = 300.0
 
 
 func _physics_process(_delta: float) -> void:
-	if Input.is_action_pressed("up"): move(Vector2.UP)
-	if Input.is_action_pressed("down"): move(Vector2.DOWN)
-	if Input.is_action_pressed("left"): move(Vector2.LEFT)
-	if Input.is_action_pressed("right"): move(Vector2.RIGHT)
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+		)
+	move(input_direction)
 
 func move(direction: Vector2):
-	# attempt to move in direction
 	print(direction)
