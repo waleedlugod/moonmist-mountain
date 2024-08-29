c2413e879876f1f3c1a8a4a7a1319e1b506257d4
Waleed Lugod <lugodwaleed@gmail.com>
2024-08-29 23:49:38 +0800

    Implement basic tile-based movement

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const TILE_SIZE = 16
+var is_moving = false
 
 
+# FIX: refactor to _input() or _unhandled_input() because of diagonal
+# movement bug
 func _physics_process(_delta: float) -> void:
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
@@ -12,4 +15,15 @@ func _physics_process(_delta: float) -> void:
 	move(input_direction)
 
 func move(direction: Vector2):
-	print(direction)
+	# do not interrupt current movement
+	# FIX: probably direction == (0,0) after refactor
+	if !is_moving && direction != Vector2(0, 0):
+		var new_position = position + direction * TILE_SIZE
+
+		var move_tween = create_tween()
+		move_tween.tween_property(self, "position", new_position, 0.2).set_trans(Tween.TRANS_LINEAR)
+
+		is_moving = true
+
+		await move_tween.finished
+		is_moving = false
