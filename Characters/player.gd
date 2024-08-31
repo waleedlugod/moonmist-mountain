738597570c2257b5c2cf2f15259a97618b933bc1
Waleed Lugod <lugodwaleed@gmail.com>
2024-08-31 19:57:45 +0800

    Fix movement bugs, glitchy for 0.1 SPEED

--- Characters/player.gd
+++ Characters/player.gd
@@ -2,28 +2,29 @@ extends CharacterBody2D
 
 
 const TILE_SIZE = 16
-var is_moving = false
+const SPEED = 0.2
+var moving_direction = Vector2.ZERO
 
 
-# FIX: refactor to _input() or _unhandled_input() because of diagonal
-# movement bug
 func _physics_process(_delta: float) -> void:
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-		)
+	var input_direction = Input.get_vector("left", "right", "up", "down")
 	move(input_direction)
 
-func move(direction: Vector2):
-	# do not interrupt current movement
-	# FIX: probably direction == (0,0) after refactor
-	if !is_moving && direction != Vector2(0, 0):
-		var new_position = position + direction * TILE_SIZE
+func move(input_direction: Vector2) -> void:
+	# attempt to move if not currently moving and is a valid direction
+	if moving_direction == Vector2.ZERO \
+	and input_direction != Vector2.ZERO \
+	and (input_direction.x == 0 || input_direction.y == 0):
+		# determine target position
+		moving_direction = input_direction
+		var target_position = position + moving_direction * TILE_SIZE
 
+		# tween to new position
 		var move_tween = create_tween()
-		move_tween.tween_property(self, "position", new_position, 0.2).set_trans(Tween.TRANS_LINEAR)
-
-		is_moving = true
-
-		await move_tween.finished
-		is_moving = false
+		move_tween.tween_property(
+			self,
+			"position",
+			target_position,
+			SPEED
+			).set_trans(Tween.TRANS_LINEAR)
+		move_tween.tween_callback(func(): moving_direction = Vector2.ZERO)
