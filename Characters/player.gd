7f92d15b6ef91ee584d556c300bc574adece2914
Waleed Lugod <lugodwaleed@gmail.com>
2024-08-31 21:12:44 +0800

    Refactor movement script

--- Characters/player.gd
+++ Characters/player.gd
@@ -3,23 +3,22 @@ extends CharacterBody2D
 
 const TILE_SIZE = 16
 const SPEED = 0.2
-var moving_direction = Vector2.ZERO
+var is_moving = false
 
 
-func _physics_process(_delta: float) -> void:
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	move(input_direction)
+func _process(_delta: float) -> void:
+	move(Input.get_vector("left", "right", "up", "down"))
 
 func move(input_direction: Vector2) -> void:
 	# attempt to move if not currently moving and is a valid direction
-	if moving_direction == Vector2.ZERO \
+	if not is_moving \
 	and input_direction != Vector2.ZERO \
 	and (input_direction.x == 0 || input_direction.y == 0):
 		# determine target position
-		moving_direction = input_direction
-		var target_position = position + moving_direction * TILE_SIZE
+		var target_position = position + input_direction * TILE_SIZE
 
-		# tween to new position
+		# move to new position
+		is_moving = true
 		var move_tween = create_tween()
 		move_tween.tween_property(
 			self,
@@ -27,4 +26,4 @@ func move(input_direction: Vector2) -> void:
 			target_position,
 			SPEED
 			).set_trans(Tween.TRANS_LINEAR)
-		move_tween.tween_callback(func(): moving_direction = Vector2.ZERO)
+		move_tween.tween_callback(func(): is_moving = false)
