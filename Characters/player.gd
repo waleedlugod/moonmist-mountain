7eac0fee4312c64bc8cb33831bbd0357ef27434b
Waleed Lugod <lugodwaleed@gmail.com>
2024-09-01 17:49:34 +0800

    Implement collision detection

--- Characters/player.gd
+++ Characters/player.gd
@@ -6,24 +6,28 @@ const SPEED = 0.2
 var is_moving = false
 
 
-func _process(_delta: float) -> void:
-	move(Input.get_vector("left", "right", "up", "down"))
+func _physics_process(_delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
 
-func move(input_direction: Vector2) -> void:
-	# attempt to move if not currently moving and is a valid direction
 	if not is_moving \
-	and input_direction != Vector2.ZERO \
-	and (input_direction.x == 0 || input_direction.y == 0):
-		# determine target position
-		var target_position = position + input_direction * TILE_SIZE
+	and direction != Vector2.ZERO \
+	and (direction.x == 0 || direction.y == 0):
+		# Query the target position if it is walkable
+		var space_state = get_world_2d().direct_space_state
+		var query_target = PhysicsPointQueryParameters2D.new()
+		query_target.position = global_position + direction * TILE_SIZE
+		var collision = space_state.intersect_point(query_target)
 
-		# move to new position
-		is_moving = true
-		var move_tween = create_tween()
-		move_tween.tween_property(
-			self,
-			"position",
-			target_position,
-			SPEED
-			).set_trans(Tween.TRANS_LINEAR)
-		move_tween.tween_callback(func(): is_moving = false)
+		if not collision: move(query_target.position)
+
+# move player to target position
+func move(target_position: Vector2) -> void:
+	is_moving = true
+	var move_tween = create_tween()
+	move_tween.tween_property(
+		self,
+		"position",
+		target_position,
+		SPEED
+		).set_trans(Tween.TRANS_LINEAR)
+	move_tween.tween_callback(func(): is_moving = false)
